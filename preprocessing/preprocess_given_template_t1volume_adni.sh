4eec17eb4fa786417d8c59164d1671f035bc62b8
basrizk <basem.rizk@outlook.com>
2023-04-13 17:40:41 -0700

    fix batch_job template + write env for slurm as default + rewrite t1volume_adni.sh for carc

--- preprocessing/preprocess_given_template_t1volume_adni.sh
+++ preprocessing/preprocess_given_template_t1volume_adni.sh
@@ -1,14 +1,15 @@
 . ./preprocessing/source_env.sh
 
 # Modify if necessary
-NUM_PROCS=1
+NUM_PROCS=32
 MODE='Train' # Test/Val/Train
-ADNI_CONVERTED_DIR='data/SUBSET_OF_DATA'
+PREFIX_DATA_DIR='/project/ajiteshs_1045/alzhiemers'
+ADNI_CONVERTED_DIR='/scratch1/ag_394/ADNI3_MRI_BIDS'
 
 
 CAPS_DIR='TRAIN'
 TMP_WORK_DIR=./WD_${MODE}
-CLINICAL_DATA_DIRECTORY="data/clinical_data_merged"
+CLINICAL_DATA_DIRECTORY="${PREFIX_DATA_DIR}/clinical_data_merged"
 PROCESSED_DIR="${ADNI_CONVERTED_DIR}_PROCESSED"
 TSV_DIR=splits/${MODE}_ADNI.tsv
 
